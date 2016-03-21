ff69b98acc8c5e80538b2e240e9a342619b1c86d
Joachim <joachimdamandersen@gmail.com>
2016-03-22 00:03:14 +0100

    First draft of game

--- MayerZeGame/app.js
+++ MayerZeGame/app.js
@@ -3,10 +3,32 @@ var app = express();
 var http = require("http");
 var io = require("socket.io");
 
+var io = new io();
+
+var server = http.createServer(app);
+io.listen(server);
+
 app.get("/", function(req, res) {
-  res.send("yes. det virker. fedt");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function() {
+var game = {
+  players: []
+};
+
+io.on("connection", function(socket) {
+  console.log("a user connected!");
+  game.players.push(socket);
+  console.log("player array size:", game.players.length);
+  console.log(game);
+  socket.on("disconnect", function() {
+    console.log("mofo disconnected");
+    var index = game.players.indexOf(socket);
+    game.players.splice(index, 1);
+    console.log("player array size:", game.players.length);
+  });
+});
+
+server.listen(3000, function() {
   console.log("Jeg lytter..");
 });
